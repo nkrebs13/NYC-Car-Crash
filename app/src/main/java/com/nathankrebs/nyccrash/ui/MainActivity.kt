8517018e24299d1ff453dc28c5610a35c0a4c93d
Nathan Krebs <nathan.krebs@deliverhealth.com>
2023-03-22 15:11:14 -0700

    working graph -- wip

--- app/src/main/java/com/nathankrebs/nyccrash/ui/MainActivity.kt
+++ app/src/main/java/com/nathankrebs/nyccrash/ui/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomSheetScaffold
@@ -15,11 +17,14 @@ import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.nathankrebs.nyccrash.ui.compose.AppMap
+import com.nathankrebs.nyccrash.ui.compose.HourlyGraph
 import com.nathankrebs.nyccrash.ui.theme.NYCCrashTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
 
@@ -43,9 +48,30 @@ class MainActivity : ComponentActivity() {
                     sheetContent = {
                         Column(
                             modifier = Modifier
-                                .heightIn(max = 64.dp)
+//                                .heightIn(max = 64.dp)
                                 .verticalScroll(state = rememberScrollState()),
                         ) {
+                            val yAxisCount = 8
+                            val yStep: Int = remember(carCrashState.value.crashesByTime) {
+                                val maxCrashes = carCrashState.value.crashesByTime.max()
+                                val chartMax = maxCrashes * 1.1
+                                (chartMax / yAxisCount).roundToInt()
+                            }
+                            val yValues: List<String> = remember(yStep) {
+                                IntRange(0, yAxisCount - 1).map { (it * yStep).toString() }
+                            }
+                            HourlyGraph(
+                                modifier = Modifier
+                                    .height(256.dp)
+                                    .padding(16.dp),
+                                points = carCrashState.value.crashesByTime.toList(),
+                                // todo - move these to res
+                                xValues = listOf("midnight", "6am", "noon", "6pm"),
+                                yValues = yValues,
+                                paddingSpace = 32.dp,
+                                verticalStep = yStep
+                            )
+
                             Text(text = carCrashState.value.toString())
                         }
                     },
---
+++ app/src/main/java/com/nathankrebs/nyccrash/ui/compose/HourlyGraph.kt
@@ -0,0 +1,202 @@
+package com.nathankrebs.nyccrash.ui.compose
+
+
+import android.graphics.Paint
+import android.graphics.PointF
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Created by Saurabh
+ */
+@Composable
+fun HourlyGraph(
+    modifier: Modifier,
+    points: List<Int>,
+    xValues: List<String>,
+    yValues: List<String>,
+    paddingSpace: Dp,
+    verticalStep: Int
+) {
+    val controlPoints1 = mutableListOf<PointF>()
+    val controlPoints2 = mutableListOf<PointF>()
+    val coordinates = mutableListOf<PointF>()
+    val density = LocalDensity.current
+    val textPaint = remember(density) {
+        Paint().apply {
+            color = android.graphics.Color.BLACK
+            textAlign = Paint.Align.CENTER
+            textSize = density.run { 12.sp.toPx() }
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .background(Color.White)
+            .padding(horizontal = 8.dp, vertical = 12.dp),
+        contentAlignment = Center
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
+            val xPointDistance = (size.width - paddingSpace.toPx()) / points.size
+            val yAxisSpace = size.height / yValues.size
+            /** placing x axis points */
+            for (i in xValues.indices) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    xValues[i],
+                    xAxisSpace * i + paddingSpace.toPx(),
+                    size.height - 30,
+                    textPaint
+                )
+            }
+            /** placing y axis points */
+            for (i in yValues.indices) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    yValues[i],
+                    paddingSpace.toPx() / 2f,
+                    size.height - yAxisSpace * (i + 1),
+                    textPaint
+                )
+            }
+            /** placing our x axis points */
+            for (i in points.indices) {
+                val x1 = xPointDistance * i + paddingSpace.toPx()
+                val y1 = size.height - (yAxisSpace * (points[i] / verticalStep.toFloat()))
+                coordinates.add(PointF(x1, y1))
+                /** drawing circles to indicate all the points */
+                drawCircle(
+                    color = Color.Red,
+                    radius = 10f,
+                    center = Offset(x1, y1)
+                )
+            }
+            /** calculating the connection points */
+            for (i in 1 until coordinates.size) {
+                controlPoints1.add(
+                    PointF(
+                        (coordinates[i].x + coordinates[i - 1].x) / 2,
+                        coordinates[i - 1].y
+                    )
+                )
+                controlPoints2.add(
+                    PointF(
+                        (coordinates[i].x + coordinates[i - 1].x) / 2,
+                        coordinates[i].y
+                    )
+                )
+            }
+//
+//            listOf(controlPoints1, controlPoints2).forEach {
+//                it.forEach { controlPoint ->
+//                    drawCircle(
+//                        color = Color.Blue.copy(alpha = 0.7f),
+//                        radius = 7f,
+//                        center = Offset(controlPoint.x, controlPoint.y)
+//                    )
+//                }
+//            }
+            controlPoints1[1].let { controlPoint ->
+                drawCircle(
+                    color = Color.Blue.copy(alpha = 0.7f),
+                    radius = 7f,
+                    center = Offset(controlPoint.x, controlPoint.y)
+                )
+            }
+            controlPoints2[1].let { controlPoint ->
+                drawCircle(
+                    color = Color.Blue.copy(alpha = 0.7f),
+                    radius = 7f,
+                    center = Offset(controlPoint.x, controlPoint.y)
+                )
+            }
+            /** drawing the path */
+            val stroke = Path().drawBezierCurve(coordinates, controlPoints1, controlPoints2)
+//            val stroke = Path().apply {
+//                reset()
+//                moveTo(coordinates.first().x, coordinates.first().y)
+//                for (i in 0 until coordinates.size - 1) {
+//                    cubicTo(
+//                        controlPoints1[i].x, controlPoints1[i].y,
+//                        controlPoints2[i].x, controlPoints2[i].y,
+//                        coordinates[i + 1].x, coordinates[i + 1].y
+//                    )
+//                }
+//            }
+            /** filling the area under the path */
+//            val fillPath = android.graphics.Path(stroke.asAndroidPath())
+//                .asComposePath()
+//                .apply {
+//                    lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
+//                    lineTo(xAxisSpace, size.height - yAxisSpace)
+//                    close()
+//                }
+//            drawPath(
+//                fillPath,
+//                brush = Brush.verticalGradient(
+//                    listOf(
+//                        Color.Cyan,
+//                        Color.Transparent,
+//                    ),
+//                    endY = size.height - yAxisSpace
+//                ),
+//            )
+            drawPath(
+                stroke,
+                color = Color.Black,
+                style = Stroke(
+                    width = 5f,
+                    cap = StrokeCap.Round
+                )
+            )
+        }
+    }
+}
+
+private fun Path.drawBezierCurve(
+    points: List<PointF>,
+    conPoints1: List<PointF>,
+    conPoints2: List<PointF>,
+): Path = this.apply {
+    this.reset()
+    this.moveTo(points.first().x, points.first().y)
+    this.cubicTo(conPoints1[0], conPoints2[0], points[1])
+    this.cubicTo(conPoints1[1], conPoints2[1], points[2])
+    this.cubicTo(conPoints1[2], conPoints2[2], points[3])
+    for(i in 4 until points.size) {
+        this.cubicTo(conPoints1[i - 1], conPoints2[i - 1], points[i])
+    }
+//    for(i in 1 until points.size) {
+//        this.cubicTo(
+//            // previous conPoint
+//            conPoints1[i - 1].x, conPoints1[i-1].y,
+//            conPoints2[i - 1].x, conPoints2[i-1].y,
+//            points[i].x, points[i].y
+//        )
+//    }
+}
+
+fun Path.cubicTo(point1: PointF, point2: PointF, point3: PointF) {
+    this.cubicTo(
+        point1.x, point1.y,
+        point2.x, point2.y,
+        point3.x, point3.y
+    )
+}
